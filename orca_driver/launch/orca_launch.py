94480c1603c7aebb93ce53393da668afc05b3538
Clyde McQueen <clyde@mcqueen.net>
2019-05-02 20:08:17 -0700

    Real launch file

--- orca_driver/launch/orca_launch.py
+++ orca_driver/launch/orca_launch.py
@@ -4,12 +4,12 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-# OpenCV camera launch, useful for testing the image processing pipeline
+# Orca launch
 
 
 def generate_launch_description():
     # Must match camera name in URDF file
-    left_camera_name = 'left_camera'
+    main_camera_name = 'left_camera'  # TODO
 
     urdf = os.path.join(get_package_share_directory('orca_description'), 'urdf', 'orca.urdf')
 
@@ -18,21 +18,32 @@ def generate_launch_description():
         Node(package='robot_state_publisher', node_executable='robot_state_publisher', output='screen',
              arguments=[urdf]),
 
-        # OpenCV camera node (provides video)
+        # Main camera
         Node(package='orca_driver', node_executable='opencv_camera_node', output='screen',
              node_name='opencv_camera_node', remappings=[
-                ('image_raw', '/' + left_camera_name + '/image_raw'),
-                ('camera_info', '/' + left_camera_name + '/camera_info'),
+                ('image_raw', '/' + main_camera_name + '/image_raw'),
+                ('camera_info', '/' + main_camera_name + '/camera_info'),
             ]),
 
-        # # Joystick driver, generates /namespace/joy messages
-        # Node(package='joy', node_executable='joy_node', output='screen'),
+        # Bar30 node
+        Node(package='orca_driver', node_executable='bar30_node.py', output='screen'),
+
+        # Driver node handles thrusters, lights and camera tilt
+        Node(package='orca_driver', node_executable='driver_node', output='screen',
+             node_name='driver_node', parameters=[{
+                'voltage_multiplier': 5.05,
+                'thruster_4_reverse': True,                 # Thruster 4 ESC is programmed incorrectly
+                'tilt_channel': 6,
+                'voltage_min': 0                            # For bench testing TODO remove
+            }]),
 
         # AUV controller
-        # Node(package='orca_base', node_executable='base_node', output='screen',
-        #      node_name='base_node', remappings=[
-        #         ('filtered_odom', '/' + left_camera_name + '/base_odom')
-        #     ]),
+        Node(package='orca_base', node_executable='base_node', output='screen',
+             node_name='base_node', parameters=[{
+                'simulation': False
+            }], remappings=[
+                ('filtered_odom', '/' + main_camera_name + '/base_odom')
+            ]),
 
         # Mapper
         Node(package='fiducial_vlam', node_executable='vmap_node', output='screen',
@@ -43,7 +54,7 @@ def generate_launch_description():
 
         # Localizer
         Node(package='fiducial_vlam', node_executable='vloc_node', output='screen',
-             node_name='vloc_node', node_namespace=left_camera_name, parameters=[{
+             node_name='vloc_node', node_namespace=main_camera_name, parameters=[{
                 'publish_tfs': 1,                           # Publish map=>base_link and map=>left_camera_frame
                 'publish_camera_pose': 0,
                 'publish_base_pose': 0,
