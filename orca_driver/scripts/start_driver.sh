2a11ca8450ad12fcbb21de76235d747e7451303a
Clyde McQueen <clyde@mcqueen.net>
2019-06-04 14:41:52 -0700

    systemd service plus screen

---
+++ orca_driver/scripts/start_driver.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Start orca ROS driver
+
+# To install:           sudo cp orca_driver.service /lib/systemd/system
+# To start:             sudo systemctl start orca_driver.service
+# To stop:              sudo systemctl stop orca_driver.service
+# To start on boot:     sudo systemctl enable orca_driver.service
+# To not start on boot: sudo systemctl disable orca_driver.service
+
+screen -dmS orca bash -c "cd ~/ros2/orca_ws; . setup.bash; ros2 launch orca_driver pt2_launch.py; exec bash"
