15d1020e73c6d724c57a32a0cd90f64572ebb326
Clyde McQueen <clyde@mcqueen.net>
2020-04-18 11:24:52 -0700

    Driver tweaks

--- orca_driver/launch/test_launch.py
+++ orca_driver/launch/test_launch.py
@@ -7,15 +7,12 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Driver
-        Node(package='orca_driver', node_executable='driver_node', output='log',
-             node_name='driver_node', parameters=[{
-                'voltage_multiplier': 5.05,
-                'thruster_4_reverse': True,  # Thruster 4 ESC is programmed incorrectly TODO ?
-                'tilt_channel': 6,
-                'voltage_min': 0.0  # 0.0 For bench testing TODO
-            }]),
+        Node(package='orca_driver', node_executable='barometer_node', output='screen', node_name='barometer_node'),
 
-        # Tester
+        Node(package='orca_driver', node_executable='driver_node', output='screen', node_name='driver_node',
+             parameters=[{
+                 'thruster_4_reverse': True  # Thruster 4 ESC is programmed incorrectly TODO ?
+             }]),
+
         Node(package='orca_driver', node_executable='test_node', output='screen'),
     ])
--- orca_driver/scripts/start_driver.sh
+++ orca_driver/scripts/start_driver.sh
@@ -8,4 +8,4 @@
 # To start on boot:     sudo systemctl enable orca_driver.service
 # To not start on boot: sudo systemctl disable orca_driver.service
 
-screen -dmS orca bash -c "cd ~/ros2/orca_ws; . setup.bash; ros2 launch orca_driver sub_launch.py; exec bash"
+screen -dmS orca_driver bash -c "cd ~/ros2/orca_ws; . setup.bash; ros2 launch orca_driver sub_launch.py; exec bash"
